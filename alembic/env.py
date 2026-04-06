b78c8c65138dc7f678a8ebbc6e2cc3876a8559d9
Raphael Tannous <raphaeltannous@proton.me>
2026-04-06 15:29:00 +0300

    Create migration for user model

--- alembic/env.py
+++ alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from alembic import context
+from app.models.users import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
