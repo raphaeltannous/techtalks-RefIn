5f5d316314f7b504840456b7f7fe1335a4bc027c
Raphael Tannous <raphaeltannous@proton.me>
2026-04-09 21:07:31 +0300

    Remove commented line from user_project.py

--- app/models/user_project.py
+++ app/models/user_project.py
@@ -64,7 +64,6 @@ class UserProject(UserProjectBase, table=True):
         },
     )
 
-    # Hi hello
     user_profile: "UserProfile" = Relationship(
         back_populates="user_projects",
     )
