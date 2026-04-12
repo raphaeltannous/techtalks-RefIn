7c5476ebc7540aab7598738f46c85d3ccf9b1cab
Raphael Tannous <raphaeltannous@proton.me>
2026-04-12 15:28:42 +0300

    Delete app/routers/login.py and users.py

--- app/routers/main.py
+++ app/routers/main.py
@@ -1,8 +1,8 @@
+from config import settings
 from fastapi import APIRouter
 
-from . import login, users
+from .v1.main import api_v1_router
 
 api_router = APIRouter()
 
-
-api_router.include_router(login.router)
+api_router.include_router(api_v1_router, prefix=settings.API_VERSION_STRING)
