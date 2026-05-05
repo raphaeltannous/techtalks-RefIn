2add39a5f842a61092d442184e2dfb056f477ef2
Raphael Tannous <raphaeltannous@proton.me>
2026-05-05 18:27:31 +0300

    Create update_profile_picture endpoint

--- app/routers/v1/user_profile/user_profile.py
+++ app/routers/v1/user_profile/user_profile.py
@@ -1,6 +1,8 @@
 from typing import Annotated, Any
 
-from fastapi import APIRouter, Depends
+from config import settings
+from exceptions import UserProfileUploadSizeExceededError
+from fastapi import APIRouter, Depends, File, UploadFile
 from models.user_profile import UserProfile, UserProfilePublic, UserProfileUpdate
 from routers.dependencies import get_current_user_profile, get_user_profile_service
 from services.user_profile import UserProfileService
@@ -41,4 +43,26 @@ def update_profile(
     return user_profile_service.update_profile(
         user_profile=user_profile,
         profile_in=profile_in,
+    )
+
+
+@router.patch(
+    "/profile-picture",
+    response_model=UserProfilePublic,
+)
+async def upload_profile_picture(
+    user_profile_service: Annotated[
+        UserProfileService, Depends(get_user_profile_service)
+    ],
+    user_profile: Annotated[UserProfile, Depends(get_current_user_profile)],
+    file: UploadFile = File(...),
+):
+    contents = await file.read()
+
+    if len(contents) > settings.MAX_USER_PROFILE_UPLOAD_SIZE_BYTES:
+        raise UserProfileUploadSizeExceededError()
+
+    return user_profile_service.update_profile_picture(
+        user_profile=user_profile,
+        image_bytes=contents,
     )
