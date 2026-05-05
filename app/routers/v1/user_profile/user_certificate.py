d9775d1a0a7d12c6ed7b4564f9c63e81d937faa9
Raphael Tannous <raphaeltannous@proton.me>
2026-05-06 01:04:48 +0300

    Update user certificate router

--- app/routers/v1/user_profile/user_certificate.py
+++ app/routers/v1/user_profile/user_certificate.py
@@ -3,13 +3,13 @@ from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends
 from models.message import Message
+from models.user_profile import UserProfile
 from models.user_certificate import (
     UserCertificateIn,
     UserCertificatePublic,
     UserCertificatesPublic,
     UserCertificateUpdate,
 )
-from models.user_profile import UserProfile
 from routers.dependencies import get_current_user_profile, get_user_profile_service
 from services.user_profile import UserProfileService
 
@@ -22,15 +22,37 @@ router = APIRouter(
     "/by-username/{username}",
     response_model=UserCertificatesPublic,
 )
-def get_user_certificates(
+def get_all_by_username(
     *,
     user_profile_service: Annotated[
         UserProfileService, Depends(get_user_profile_service)
     ],
     username: str,
 ) -> Any:
-    return user_profile_service.get_all_certificates_by_username(
+    """
+    Get all user certificates by username.
+    """
+    return user_profile_service.certificate_service.get_all_by_username(
         username=username,
+    )
+
+
+@router.get(
+    "/{certificate_id}",
+    response_model=UserCertificatePublic,
+)
+def get_by_id(
+    *,
+    user_profile_service: Annotated[
+        UserProfileService, Depends(get_user_profile_service)
+    ],
+    certificate_id: uuid.UUID,
+) -> Any:
+    """
+    Get user certificate by id.
+    """
+    return user_profile_service.certificate_service.get_by_id(
+        certificate_id=certificate_id,
     )
 
 
@@ -38,7 +60,7 @@ def get_user_certificates(
     "/",
     response_model=UserCertificatePublic,
 )
-def add_certificate(
+def add(
     *,
     user_profile_service: Annotated[
         UserProfileService, Depends(get_user_profile_service)
@@ -49,25 +71,9 @@ def add_certificate(
     """
     Add new user certificate.
     """
-    return user_profile_service.add_certificate(
+    return user_profile_service.certificate_service.add(
         user_profile=user_profile,
         certificate_in=certificate_in,
-    )
-
-
-@router.get(
-    "/{certificate_id}",
-    response_model=UserCertificatePublic,
-)
-def get_certificate_by_id(
-    *,
-    user_profile_service: Annotated[
-        UserProfileService, Depends(get_user_profile_service)
-    ],
-    certificate_id: uuid.UUID,
-) -> Any:
-    return user_profile_service.get_certificate_by_id(
-        certificate_id=certificate_id,
     )
 
 
@@ -75,7 +81,7 @@ def get_certificate_by_id(
     "/{certificate_id}",
     response_model=UserCertificatePublic,
 )
-def update_certificate(
+def update(
     *,
     user_profile_service: Annotated[
         UserProfileService, Depends(get_user_profile_service)
@@ -84,7 +90,10 @@ def update_certificate(
     certificate_id: uuid.UUID,
     certificate_in: UserCertificateUpdate,
 ) -> Any:
-    return user_profile_service.update_certificate(
+    """
+    Update user certificate.
+    """
+    return user_profile_service.certificate_service.update(
         user_profile=user_profile,
         certificate_id=certificate_id,
         certificate_in=certificate_in,
@@ -95,7 +104,7 @@ def update_certificate(
     "/{certificate_id}",
     response_model=Message,
 )
-def delete_certificate(
+def delete(
     *,
     user_profile_service: Annotated[
         UserProfileService, Depends(get_user_profile_service)
@@ -103,10 +112,14 @@ def delete_certificate(
     user_profile: Annotated[UserProfile, Depends(get_current_user_profile)],
     certificate_id: uuid.UUID,
 ) -> Any:
-    user_profile_service.delete_certificate(
+    """
+    Delete user certificate.
+    """
+    user_profile_service.certificate_service.delete(
         user_profile=user_profile,
         certificate_id=certificate_id,
     )
+
     return Message(
         message="User certificate deleted.",
     )
