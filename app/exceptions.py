0b2c993a16ad56f4fae8af13c48635691104510b
Raphael Tannous <raphaeltannous@proton.me>
2026-05-05 18:26:11 +0300

    Create new exceptions for user profile and image

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,3 +1,4 @@
+from config import settings
 from fastapi import HTTPException, status
 
 
@@ -118,4 +119,20 @@ class UserExperienceNotFoundError(HTTPException):
         super().__init__(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User experience not found",
+        )
+
+
+class UserProfileUploadSizeExceededError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"File too large. Maximum size is {settings.MAX_USER_PROFILE_UPLOAD_SIZE_BYTES / (1024 * 1024):.0f}MB",
+        )
+
+
+class InvalidImageError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid image file",
         )
