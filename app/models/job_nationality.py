c6f0b95a94a0124e7d82de606fc57d70d72b08a9
Dina Amer <dinadinaamer@gmail.com>
2026-05-09 16:54:10 +0300

    Add job nationality model

---
+++ app/models/job_nationality.py
@@ -0,0 +1,63 @@
+import uuid
+from datetime import datetime, timezone
+
+from pydantic.alias_generators import to_snake
+from sqlalchemy import DateTime
+from sqlalchemy.orm import declared_attr
+from sqlmodel import Field, SQLModel
+
+
+class JobNationalityBase(SQLModel):
+    nationality: str = Field(max_length=50)
+
+
+class JobNationality(JobNationalityBase, table=True):
+    @declared_attr.directive  # type: ignore[misc]
+    @classmethod
+    def __tablename__(cls) -> str:  # pyright: ignore[reportIncompatibleVariableOverride]
+        return to_snake(cls.__name__)
+
+    id: uuid.UUID = Field(
+        default_factory=uuid.uuid4,
+        index=True,
+        primary_key=True,
+    )
+
+    job_id: uuid.UUID = Field(
+        index=True,
+        nullable=False,
+        foreign_key="job.id",
+        ondelete="CASCADE",
+    )
+
+    created_at: datetime | None = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_type=DateTime(timezone=True),  # type: ignore
+    )
+
+    updated_at: datetime | None = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_type=DateTime(timezone=True),  # type: ignore
+        sa_column_kwargs={
+            "onupdate": lambda: datetime.now(timezone.utc),
+        },
+    )
+
+
+class JobNationalityIn(JobNationalityBase):
+    pass
+
+
+class JobNationalityUpdate(JobNationalityBase):
+    pass
+
+
+class JobNationalityPublic(JobNationalityBase):
+    id: uuid.UUID
+    job_id: uuid.UUID
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+
+
+class JobNationalitiesPublic(SQLModel):
+    nationalities: list[JobNationalityPublic]
